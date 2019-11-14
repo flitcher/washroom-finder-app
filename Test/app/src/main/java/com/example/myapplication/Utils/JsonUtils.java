bcb97f16c11e772d0da2bea1449c2acf73d54ae7
ksim <ksim@sfu.ca>
2019-11-14 00:21:01 -0800

    commented out fields

--- Test/app/src/main/java/com/example/myapplication/Utils/JsonUtils.java
+++ Test/app/src/main/java/com/example/myapplication/Utils/JsonUtils.java
@@ -18,18 +18,19 @@ public class JsonUtils {
     public static List<Washroom> parseWashroomJsonFile (String json) {
         List<Washroom> washrooms = new ArrayList<>();
         try {
-            JSONObject jsonObject = new JSONObject(json);
+            JSONObject jsonObject = new JSONObject(json);;
             JSONArray jsonArray = jsonObject.getJSONArray("washrooms");
+            System.out.println(jsonArray.length());
             for(int i = 0; i < jsonArray.length(); i++) {
                 Washroom washroom = new Washroom();
                 JSONObject fieldInfo = jsonArray.getJSONObject(i).getJSONObject("fields");
 
-                washroom.setMaintainer(checkNull(fieldInfo.getString("maintainer")));
-                washroom.setName(checkNull(fieldInfo.getString("name")));
-                washroom.setWheel_access(checkNull(fieldInfo.getString("wheel_access")));
-                washroom.setPrimaryind(checkNull(fieldInfo.getString("primaryind")));
-                washroom.setNote(checkNull(fieldInfo.getString("note")));
-                washroom.setSummer_hours(checkNull(fieldInfo.getString("summer_hours")));
+//                washroom.setMaintainer(checkNull(fieldInfo.getString("maintainer")));
+//                washroom.setName(checkNull(fieldInfo.getString("name")));
+//                washroom.setWheel_access(checkNull(fieldInfo.getString("wheel_access")));
+//                washroom.setPrimaryind(checkNull(fieldInfo.getString("primaryind")));
+//                washroom.setNote(checkNull(fieldInfo.getString("note")));
+//                washroom.setSummer_hours(checkNull(fieldInfo.getString("summer_hours")));
 
                 JSONObject geomInfo = fieldInfo.getJSONObject("geom");
                 washroom.setGeon_type(checkNull(geomInfo.getString("type")));
@@ -41,13 +42,14 @@ public class JsonUtils {
                 }
                 washroom.setCoordinates(coord);
 
-                washroom.setLocation(checkNull(fieldInfo.getString("location")));
-                washroom.setAddress(checkNull(fieldInfo.getString("address")));
-                washroom.setWinter_hours(checkNull(fieldInfo.getString("winter_hours")));
-                washroom.setType(checkNull(fieldInfo.getString("type")));
-                washroom.setGeo_local_area(checkNull(fieldInfo.getString("geo_local_area")));
+//                washroom.setLocation(checkNull(fieldInfo.getString("location")));
+//                washroom.setAddress(checkNull(fieldInfo.getString("address")));
+//                washroom.setWinter_hours(checkNull(fieldInfo.getString("winter_hours")));
+//                washroom.setType(checkNull(fieldInfo.getString("type")));
+//                washroom.setGeo_local_area(checkNull(fieldInfo.getString("geo_local_area")));
 
                 washrooms.add(washroom);
+                System.out.println(i);
             }
 
         } catch (JSONException e) {
